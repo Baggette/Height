9b11a5799e411b89a96e5496b08b31fe96216445
Baggette <rishis@live.wsd1.org>
2023-05-22 18:52:25 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace height
+{
+    class Height
+    {
+        static void Main()
+        {
+            string height;
+            Console.Write("Enter your height in feet: ");
+            height = Console.ReadLine();
+            System.Windows.Forms.MessageBox.Show("You are " + height + " feet tall.");
+            Console.ReadKey(); 
+        }
+    }
+}
